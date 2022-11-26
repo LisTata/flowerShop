3ee3106d61661acc20f6600ae7124a546c476646
LisTata <114424544+LisTata@users.noreply.github.com>
2022-11-26 22:47:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Rose rose = new Rose(Color.Pink, 3.5, 35, 3);
         Carnation carnation = new Carnation(Color.Pink, 4.5, 40, 2);
-        Tulip tulip=new Tulip(Color.Red,2.3,33,1)
+        Tulip tulip=new Tulip(Color.Red,2.3,33,1);
         Basket basket = new Basket(20);
         Bouquet bouquet = new Bouquet();
         bouquet.addFlower(rose, 3).addFlower(tulip,5).addFlower(carnation, 1).addAccessories(basket);
